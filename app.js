fced6fcf3355c897eea85a44eb46915f431556b0
matlvy <mati0184@hotmail.com>
2023-09-30 03:20:38 -0300

    Primer pre entrega Commit push

--- app.js
+++ app.js
@@ -1,54 +1,25 @@
-/* 
-let movie=prompt("What movie are you going to watch?")
-switch(movie){
-    case "the dark knight":
-        console.log("you 're going to watch The dark knight")
-        break
-        case "titanic":
-            console.log("you 're going to watch Titanic")
-        break
-        case "The Avengers":
-            console.log("you 're going to watch The Avengers")
-        break
-        default:
-            console.log("You are going to watch another alternative")
-            break
-}
 
-Backticks=Alt96 `${variable}`  
+const tipo_producto_1 = "notebook"
+const marca_producto_1 = "dell"
+let precio_producto_1 = 600000
+const forma_de_pago_1 = "cuotas"
+const forma_de_pago_2 = "contado"
+let cantidad_cuotas = 12
+const valor_cuota = precio_producto_1 / cantidad_cuotas
 
-movie=prompt("What movie are you going to watch?")
-
-switch(movie){
-    case "batman":
-        console.log(`you 're going to watch ${movie}`)
-        break
-        case "avatar":
-            console.log(`you 're going to watch ${movie}`)
-        break
-        case "The Avengers":
-            console.log(`you 're going to watch ${movie}`)
-        break
-        default:
-            console.log(`you 're going to watch ${movie}`)
-            break
-        }  
-        let i=0
-        while(i<=10){console.log(i)
-        i++
-    } 
-    let number=parseInt(prompt("Tell us a number please"))
-    for (let i=1; i<=10; i++){
-        let product=number*i
-        console.log(`${number}*${i}=${product}`)
+let marca_producto_seleccionada = prompt("Que marca de notebook desea comprar?")
+if (marca_producto_seleccionada == marca_producto_1) {
+    alert("Felicitaciones: ha decidido comprar la nueva " + tipo_producto_1 + " " + marca_producto_1 + ".")
+    let forma_de_pago_seleccionada = prompt("Ingrese forma de pago")
+    if (forma_de_pago_seleccionada == forma_de_pago_1) {
+        alert("A continuacion, procederemos al calculo de sus cuotas")
+        for (let i = 1; i <= 12; i++) {
+            console.log(`${precio_producto_1} / ${i} = ${valor_cuota}`);
+        }
+    } else if (forma_de_pago_seleccionada == forma_de_pago_2) {
+        console.log("La compra sera abonada al contado.");
     }
-    */
-const tipo_producto_1 = "Notebook"
-const marca_producto_1 = "Dell"
-
-let marca_producto_seleccionado = prompt("Que marca de notebook desea comprar?")
-if (marca_producto_seleccionado == marca_producto_1) { alert("Felicitaciones! Has decidido comprar la nueva " + tipo_producto_1 + " " + marca_producto_1) }
-else if (marca_producto_seleccionado != "") { alert("Lo sentimos. El producto que buscas no se encuentra disponible") }
+    else { console.log("Debe ingresar una opcion para continuar."); }
+}
+else if (marca_producto_seleccionada != "") { alert("Lo sentimos. El producto que buscas no se encuentra disponible") }
 else { alert("Atencion! Debes ingresar una opcion para continuar.") }
-
-let precio_producto_1 = "$600000"
