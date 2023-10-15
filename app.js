9e0bb456c9c4625f1fdca1690d26ce6e7eb3bdea
matlvy <mati0184@hotmail.com>
2023-10-15 06:54:42 -0300

    commit push segunda entrega primer parte

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 
-const tipo_producto_1 = "notebook"
-const marca_producto_1 = "dell"
-let precio_producto_1 = 600000
-const forma_de_pago_1 = "cuotas"
-const forma_de_pago_2 = "contado"
-let cantidad_cuotas = 12
-const valor_cuota = precio_producto_1 / cantidad_cuotas
-
-let marca_producto_seleccionada = prompt("Que marca de notebook desea comprar?")
-if (marca_producto_seleccionada == marca_producto_1) {
-    alert("Felicitaciones: ha decidido comprar la nueva " + tipo_producto_1 + " " + marca_producto_1 + ".")
-    let forma_de_pago_seleccionada = prompt("Ingrese forma de pago")
-    if (forma_de_pago_seleccionada == forma_de_pago_1) {
-        alert("A continuacion, procederemos al calculo de sus cuotas")
-        for (let i = 1; i <= 12; i++) {
-            console.log(`${precio_producto_1} / ${i} = ${valor_cuota}`);
-        }
-    } else if (forma_de_pago_seleccionada == forma_de_pago_2) {
-        console.log("La compra sera abonada al contado.");
-    }
-    else { console.log("Debe ingresar una opcion para continuar."); }
+/*
+function welcome() {
+    let fullname = prompt("Ingrese su nombre completo")
+    alert("Hola " + fullname + "! A continuacion, debera ingresar al sitio para continuar")
+    let username = prompt("Ingrese su usuario")
+    let email = prompt("Ingrese su email")
+    alert("Bienvenido a nuestro ecommerce " + username + "!")
 }
-else if (marca_producto_seleccionada != "") { alert("Lo sentimos. El producto que buscas no se encuentra disponible") }
-else { alert("Atencion! Debes ingresar una opcion para continuar.") }
+welcome()
+*/
+const films = [
+    id1 = { Pelicula: "Titanic", Genero: "Drama", Fecha: 1997, Rating: 7.9, Precio: 1000 },
+    id2 = { Pelicula: "Avatar", Genero: "Aventura", Fecha: 2009, Rating: 7.9, Precio: 2000 },
+    id3 = { Pelicula: "The Terminator", Genero: "Accion", Fecha: 1984, Rating: 8.1, Precio: 2000 },
+    id4 = { Pelicula: "Back to the Future", Genero: "Aventura", Fecha: 1985, Rating: 8.5, Precio: 1500 },
+    id5 = { Pelicula: "The Dark Knight", Genero: "Accion", Fecha: 2008, Rating: 9, Precio: 2500 },
+    id6 = { Pelicula: "Inception", Genero: "Sci-Fi", Fecha: 2010, Rating: 8.8, Precio: 2000 },
+    id7 = { Pelicula: "Matrix", Genero: "Sci-Fi", Fecha: 1999, Rating: 8.7, Precio: 2000 },
+    id8 = { Pelicula: "Jurassic Park", Genero: "Accion", Fecha: 1993, Rating: 8.2, Precio: 2000 },
+    id9 = { Pelicula: "The Godfather", Genero: "Crimen", Fecha: 1972, Rating: 9.2, Precio: 2000 },
+    id10 = { Pelicula: "Halloween", Genero: "Thriller", Fecha: 1978, Rating: 7.7, Precio: 1000 },
+]
+console.log(films)
+for (const film of films)
+    console.log((film.Pelicula), (film.Fecha), (film.Genero), (film.Rating),)
+films.pop()
+films.push(id11 = { Pelicula: "Star Wars", Genero: "Sci-Fi", Fecha: 1977, Rating: 8.6, Precio: 2000 })
+
+films.map((f) => { console.log(f) })
+
+let filtrar = films.filter((f) => f.Genero === "Sci-Fi")
+console.log(filtrar)
+
+let buscar = films.find((f) => f.Precio < 2000)
+console.log(buscar)
+
+let cart = []
+
+cart.push(id1, id2)
+console.log(cart)
+let purchased = cart.reduce((accum, f) => { return accum + f.Precio }, 0)
+console.log(purchased)
