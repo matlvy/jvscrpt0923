1b777331d245dbc2c1c28044ed793b0dd3fd9c8f
matlvy <mati0184@hotmail.com>
2023-11-06 01:52:25 -0300

    Tercer entrega

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 
-function welcome() {
-    let name = prompt("Ingrese su nombre")
-    { alert("Hola " + name + "! A continuacion, debera ingresar al sitio para continuar") }
-    let email = prompt("Ingrese su email")
-    if (email == "") { alert("Atencion! Debes ingresar su email para continuar.") }
-    else {
-        let password = prompt("Ingrese su contraseña")
-        if (password == "") { alert("Atencion! Debes ingresar su contraseña para continuar") }
-        else { alert("Bienvenido a nuestro ecommerce!") }
+const input1 = document.querySelector("#email")
+input1.addEventListener("input", (e) => {
+    console.log(e.target.value)
+    if (e.target.value === "") {
+        console.log("Validation failed")
     }
+})
+const input2 = document.querySelector("#password")
+input2.addEventListener("input", (e) => {
+    console.log(e.target.value)
+    if (e.target.value === "") {
+        console.log("Validation failed")
+    }
+})
+const formulario = document.querySelector("form")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+
+formulario.addEventListener("submit", validarFormulario)
+
+function validarFormulario(e) {
+    e.preventDefault()
+    console.log(`email : ${email.value}`)
+    console.log(`password: ${password.value}`)
 }
-welcome()
+const boton = document.querySelector("button")
+button.addEventListener("click", function () {
+    console.log(button.textContent)
+})
 const films = [
     id1 = { Pelicula: "Titanic", Genero: "Drama", Fecha: 1997, Rating: 7.9, Precio: 1000 },
     id2 = { Pelicula: "Avatar", Genero: "Aventura", Fecha: 2009, Rating: 7.9, Precio: 2000 },
@@ -29,6 +46,16 @@ for (const film of films)
 films.pop()
 films.push(id11 = { Pelicula: "Star Wars", Genero: "Sci-Fi", Fecha: 1977, Rating: 8.6, Precio: 2000 })
 
+
+for (let film of films) {
+    let list = document.createElement("div")
+    list.innerHTML = `
+        <h3>Titulo: ${film.Pelicula}</h3>
+        <h4>Precio: ${film.Precio}</h4>
+        <h4>Rating: ${film.Rating}</h4>
+                `
+    document.body.appendChild(list)
+}
 films.map((f) => { console.log(f) })
 
 let filter1 = films.filter((f) => f.Genero === "Sci-Fi")
@@ -43,26 +70,158 @@ console.log(buscar)
 let initial_stock = [500]
 let cart = []
 
-cart.push(id1, id2)
 console.log(cart)
 console.log(cart.length)
 
 let current_stock = initial_stock.reduce((s, accum) => { return accum - cart.length }, 0)
 console.log(current_stock)
 
-let total_compra = cart.reduce((accum, f) => { return accum + f.Precio }, 0)
-console.log(total_compra)
-
 const catalog = document.querySelector(".catalog")
 console.log(catalog)
 
-for (let film of films) {
-    let list = document.createElement("div")
-    list.innerHTML = `
-        <h3>Titulo: ${film.Pelicula}</h3>
-        <h4>Precio: ${film.Precio}</h4>
-        <h4>Rating: ${film.Rating}</h4>
-        <button type="submit" class="btn btn-success">Agregar al carrito</button>
+console.log(films)
+
+const filmsstr = JSON.stringify(films)
+console.log(filmsstr)
+localStorage.setItem("film", filmsstr)
+
+const filmsls = localStorage.getItem("film")
+console.log(filmsls)
+console.log(JSON.parse(filmsls))
+
+function itemstostr() {
+
+    const cartstr = JSON.stringify(cart)
+    console.log(cartstr)
+    localStorage.setItem("cart", cartstr)
+
+    const cartls = localStorage.getItem("cart")
+    console.log(cartls)
+    console.log(JSON.parse(cartls))
+}
+
+const myImage = new Image(200, 200);
+myImage.src = 'https://cdn.pixabay.com/photo/2014/04/02/10/53/shopping-cart-304843_640.png';
+document.body.appendChild(myImage);
+
+function cartlength() {
+    let items = document.createElement("div")
+    items.innerHTML =
         `
-    document.body.appendChild(list)
+<button type="button" class="btn btn-danger">${cart.length}</button> 
+     `
+    document.body.appendChild(myImage)
+    document.body.appendChild(items)
+    document.body.append("Usted ha decidido comprar:")
+}
+
+function resumencompra() {
+
+    let total = cart.reduce((a, b) => { return a + b.Precio }, 0)
+
+    for (let film of cart) {
+        let purchased = document.createElement("div")
+        purchased.innerHTML = `           
+    <h3>Titulo: ${film.Pelicula}<h3>
+    <h4> Precio: ${film.Precio}</h4> 
+             `
+        document.body.appendChild(purchased)
+    }
+    document.body.append("El monto total de su compra es $", total)
+}
+
+const inputid1 = document.querySelector("#buttonid1")
+inputid1.addEventListener("click", () => {
+    cart.push(id1)
+    itemstostr()
+    cartlength()
+    resumencompra()
+})
+const inputid2 = document.querySelector("#buttonid2")
+inputid2.addEventListener("click", () => {
+    cart.push(id2)
+    itemstostr()
+    cartlength()
+    resumencompra()
+})
+{
+    const inputid3 = document.querySelector("#buttonid3")
+    inputid3.addEventListener("click", () => {
+        cart.push(id3)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+} {
+    const inputid4 = document.querySelector("#buttonid4")
+    inputid4.addEventListener("click", () => {
+        cart.push(id4)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+} {
+    const inputid5 = document.querySelector("#buttonid5")
+    inputid5.addEventListener("click", () => {
+        cart.push(id5)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+} {
+    const inputid6 = document.querySelector("#buttonid6")
+    inputid6.addEventListener("click", () => {
+        cart.push(id6)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+} {
+    const inputid7 = document.querySelector("#buttonid7")
+    inputid7.addEventListener("click", () => {
+        cart.push(id7)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+} {
+    const inputid8 = document.querySelector("#buttonid8")
+    inputid8.addEventListener("click", () => {
+        cart.push(id8)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+} {
+    const inputid9 = document.querySelector("#buttonid9")
+    inputid9.addEventListener("click", () => {
+        cart.push(id9)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+} {
+    const inputid10 = document.querySelector("#buttonid10")
+    inputid10.addEventListener("click", () => {
+        cart.push(id10)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+}
+{
+    const inputid11 = document.querySelector("#buttonid11")
+    inputid11.addEventListener("click", () => {
+        cart.push(id11)
+        itemstostr()
+        cartlength()
+        resumencompra()
+    })
+}
+{
+    const inputid12 = document.querySelector("#buttonid12")
+    inputid12.addEventListener("click", () => {
+        localStorage.removeItem("cart")
+        document.body.append("Carrito vaciado.")
+    })
 }
